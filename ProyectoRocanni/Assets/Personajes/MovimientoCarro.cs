08c6c85e00959cc109c901c76805474e7c7b3afb
acarcila <acarcila98@gmail.com>
2018-12-06 23:33:54 -0500

    Se creó la carpeta para los personajes y se creó un personaje de prueba con la mecánica de avanzar, frenar y retroceder

---
+++ ProyectoRocanni/Assets/Personajes/MovimientoCarro.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCarro : MonoBehaviour
+{
+
+    public float velocidadMaxima;
+	public float velocidadMaximaReversa;
+
+    private float velocidad;
+    private enum Marcha { acelerando, frenando, desacelerando };
+    private Marcha marcha;
+
+    // Use this for initialization
+    void Start()
+    {
+        velocidad = 0f;
+        marcha = Marcha.frenando;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            marcha = Marcha.desacelerando;
+        }
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            marcha = Marcha.acelerando;
+        }
+		else
+		{
+			marcha = Marcha.frenando;
+		}
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (marcha == Marcha.acelerando)
+        {
+            velocidad = Mathf.MoveTowards(velocidad, velocidadMaxima, Time.deltaTime);
+        }
+		else if(marcha == Marcha.desacelerando)
+		{
+			velocidad = Mathf.MoveTowards(velocidad, velocidadMaximaReversa, Time.deltaTime*10);   
+		}
+		else if(marcha == Marcha.frenando)
+		{
+			velocidad = Mathf.MoveTowards(velocidad, 0f, Time.deltaTime*10);
+		}
+
+        this.GetComponent<Rigidbody>().velocity = Vector3.forward * velocidad;
+    }
+}
