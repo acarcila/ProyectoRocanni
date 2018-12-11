bfded72ec66590d7133d1fd46ad3a69ab97a9eaa
ocralo <e_lopez1996@hotmail.com>
2018-12-11 13:25:51 -0500

    se agrego la clase SceneManagenemnet de unityEngine

--- ProyectoRocanni/Assets/Interfaz/Scripts/Interfaz.cs
+++ ProyectoRocanni/Assets/Interfaz/Scripts/Interfaz.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Interfaz : MonoBehaviour {
+public class Interfaz : MonoBehaviour
+{
 
-	
-	void Start () {
-		
-	}
-	
-	
-	void Update () {
-		
-	}
-	//Funcion para salir del juego
-	public void Salir()
-	{
-		Application.Quit();
-	}
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+  
+
+    //Funcion para salir del juego
+    public void Salir()
+    {
+        Application.Quit();
+    }
 }
